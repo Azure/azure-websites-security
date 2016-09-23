7110db0e242302f97474bd77873ccc8c55a23484
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2016-09-23 11:12:23 -0700

    Better handling of secondary keys

--- src/Azure.WebSites.DataProtection/Constants.cs
+++ src/Azure.WebSites.DataProtection/Constants.cs
@@ -10,9 +10,7 @@ namespace Microsoft.Azure.Web.DataProtection
         public const string AzureWebsiteName = "WEBSITE_SITE_NAME";
         public const string AzureWebsiteInstanceId = "WEBSITE_INSTANCE_ID";
         public const string AzureWebsitePrimaryEncryptionKeyId = "AzureWebPrimaryEncryptionKey";
-        public const string AzureWebsiteSecondaryEncryptionKeyId = "AzureWebSecondaryEncryptionKey";
         public const string AzureWebsiteLocalEncryptionKey = "AzureWebEncryptionKey";
-        public const string AzureWebsiteEncryptionKeyId = "AzureWebDefaultEncryptionKeyId";
         public const string AzureWebReferencedKeyPrefix = "AzureWebEncryptionKey_";
         internal const string RootWebConfigPath = @"D:\local\config\rootweb.config";
         internal const string DefaultEncryptionKeyId = "00000000-0000-0000-0000-000000000000";

9f774a287fee6a545b93ab4dca90481465e3ab1c
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2017-03-17 16:47:18 -0700

    Updating package details

--- src/Azure.WebSites.DataProtection/Properties/AssemblyInfo.cs
+++ src/Azure.WebSites.DataProtection/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Azure.WebSites.DataProtection")]
+[assembly: AssemblyProduct("Microsoft.Azure.WebSites.DataProtection")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
